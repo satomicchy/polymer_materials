2e7910ad10b83580e6fc958aeb5a0e872e78accd
satomicchy <satomi_t_sora@yahoo.co.jp>
2015-03-14 11:49:16 +0900

    add test user seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,64 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+test_users =
+[
+  {
+    "uid"=> 1,
+    "text" => "Have you heard about the Web Components revolution?",
+    "username" => "Eric",
+    "avatar" => "../images/avatar-01.svg",
+    "favorite"=> false
+  },
+  {
+    "uid"=> 2,
+    "text" => "Loving this Polymer thing.",
+    "username" => "Rob",
+    "avatar" => "../images/avatar-02.svg",
+    "favorite"=> false
+  },
+    {
+    "uid"=> 3,
+    "text" => "So last year...",
+    "username" => "Dimitri",
+    "avatar" => "../images/avatar-03.svg",
+    "favorite"=> false
+  },
+  {
+    "uid"=> 4,
+    "text" => "Pretty sure I came up with that first.",
+    "username" => "Ada",
+    "avatar" => "../images/avatar-07.svg",
+    "favorite"=> false
+  },
+  {
+    "uid"=> 5,
+    "text" => "Yo, I heard you like components, so I put a component in your component.",
+    "username" => "Grace",
+    "avatar" => "../images/avatar-08.svg",
+    "favorite"=> false
+  },
+  {
+    "uid"=> 6,
+    "text" => "Centralize, centrailize.",
+    "username" => "John",
+    "avatar" => "../images/avatar-04.svg",
+    "favorite"=> false
+  },
+  {
+    "uid"=> 7,
+    "text" => "Has anyone seen my cat?",
+    "username" => "Zelda",
+    "avatar" => "../images/avatar-06.svg",
+    "favorite"=> false
+  },
+  {
+    "uid"=> 8,
+    "text" => "Decentralize!",
+    "username" => "Norbert",
+    "avatar" => "../images/avatar-05.svg",
+    "favorite"=> false
+  }
+]
+
+test_users.each do |user|
+  user = User.find_or_initialize_by(name: user["username"], memo: user["text"])
+  user.save
+end
